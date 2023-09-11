048ebeca548c547fd8a933cfe33bb054b929b673
Rachael <rachael.brady@theanalyticalmoose.com>
2023-09-10 19:14:58 -0700

    complete

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const batteryCounts = [10, 5, 3, 8, 5];  // This totals up to 31
+
+// Reducer function to sum up battery counts
+const totalBatteries = batteryCounts.reduce((total, count) => total + count, 0);
+
+// Provided test suite
+describe('reducer', function() {
+  describe('batteries', function() {
+    it('should have a `totalBatteries` variable', function() {
+      expect(totalBatteries).to.exist;
+    });
+
+    it('should have a number as a result', function() {
+      expect(totalBatteries).to.be.a('number');
+    });
+
+    it('should have made the sum of all the assembled batteries', function() {
+      expect(totalBatteries).to.eql(31);
+    });
+  });
+});
